6edf81cab9c8a99e0a84ec542faaccfdd3cf0aad
tomstrummer <tomstrummer@f908d50e-8e36-0410-ac0a-93754bd5008d>
2011-03-21 21:21:53 +0000

    GMOD-168 fixed RegistryTest for content type parameters
    git-svn-id: http://svn.codehaus.org/gmod/httpbuilder/trunk@918 f908d50e-8e36-0410-ac0a-93754bd5008d

--- src/test/groovy/groovyx/net/http/RegistryTest.groovy
+++ src/test/groovy/groovyx/net/http/RegistryTest.groovy
@@ -48,44 +48,44 @@ public class RegistryTest {
 	@Test public void testXMLEncoder() {
 		def reg = new EncoderRegistry();
 		
-		def entity = reg.encodeXML {
+		def entity = reg.encodeXML( {
 			xml( AAA: 'aaa' ) {
 				one 'one'
 				two 'two'
 			}
-		}
+		}, 'text/xml' )
 		
-		assert entity.contentType.value == "application/xml"
+		assert entity.contentType.value == "text/xml"
 //		println entity.content.text
 		assert entity.content.text == "<xml AAA='aaa'><one>one</one><two>two</two></xml>"
 		
-		entity = reg.encodeXML( '<xml AAA="aaa"><one>one</one><two>two</two></xml>' )
+		entity = reg.encodeXML( '<xml AAA="aaa"><one>one</one><two>two</two></xml>',null )
 		assert entity.content.text == '<xml AAA="aaa"><one>one</one><two>two</two></xml>'
 		def value = 'something'
-		entity = reg.encodeXML( "<xml AAA='aaa'><one>$value</one><two>two</two></xml>" )
+		entity = reg.encodeXML( "<xml AAA='aaa'><one>$value</one><two>two</two></xml>",null )
 		assert entity.content.text == "<xml AAA='aaa'><one>something</one><two>two</two></xml>"
 	}
 	
 	@Test public void testCharsetAndText() {
 		def reg = new EncoderRegistry( charset: "ISO-8859-1" );
 		
-		def entity = reg.encodeText { out ->
+		def entity = reg.encodeText( { out ->
 			out << "This is a test"
-		}
+		}, null )
 		
 		assert entity.contentType.value == 'text/plain'
 		assert entity.content.getText('ISO-8859-1') == "This is a test"
 		assert entity.content.getText('utf-16') != "This is a test"
 		
 		def w = { it << "this is a test 1" } as Writable
-		entity = reg.encodeText( w )
+		entity = reg.encodeText( w, 'text/plain' )
 		assert entity.content.getText('ISO-8859-1') == "this is a test 1"
 		
-		entity = reg.encodeText( "This is a test 2" )
-		assert entity.contentType.value == 'text/plain'
+		entity = reg.encodeText( "This is a test 2", 'text/notplain' )
+		assert entity.contentType.value == 'text/notplain'
 		assert entity.content.getText('ISO-8859-1') == "This is a test 2"
 
-		entity = reg.encodeText( new StringReader( "This is a test 3\nMore text") )
+		entity = reg.encodeText( new StringReader( "This is a test 3\nMore text"), 'text/plain' )
 		assert entity.contentType.value == 'text/plain'		
 		assert entity.content.getText('ISO-8859-1') == "This is a test 3\nMore text"
 	}
@@ -95,9 +95,9 @@ public class RegistryTest {
 		
 		def data = [ 0x0, 0x1, 0x2 ] as byte[]
 		
-		def entity = reg.encodeStream { out ->  // closure
+		def entity = reg.encodeStream( { out ->  // closure
 			out << data
-		}
+		}, null )
 		
 		assert entity.contentType.value == 'application/octet-stream'
 		assert entity.contentLength == data.length
@@ -105,19 +105,20 @@ public class RegistryTest {
 		result << entity.content
 		assert result.toByteArray() == data
 		
-		entity = reg.encodeStream( new ByteArrayInputStream(data) )
+		entity = reg.encodeStream( new ByteArrayInputStream(data), 'application/x-gzip' )
+		assert entity.contentLength == data.length
+		assert entity.contentType.value == 'application/x-gzip'
+		result = new ByteArrayOutputStream() 
+		result << entity.content
+		assert result.toByteArray() == data
+		
+		entity = reg.encodeStream( data, null ) // byte[]
 		assert entity.contentLength == data.length
 		result = new ByteArrayOutputStream() 
 		result << entity.content
 		assert result.toByteArray() == data
 		
-		entity = reg.encodeStream( data ) // byte[]
-		assert entity.contentLength == data.length
-		result = new ByteArrayOutputStream() 
-		result << entity.content
-		assert result.toByteArray() == data
-		
-		entity = reg.encodeStream( result ) // ByteArrayOutputStream
+		entity = reg.encodeStream( result, null ) // ByteArrayOutputStream
 		assert entity.contentLength == data.length
 		result = new ByteArrayOutputStream() 
 		result << entity.content
@@ -130,16 +131,17 @@ public class RegistryTest {
 		def entity = reg.encodeJSON( [
 				first : [ one:1, two:"2" ],
 				second : 'some string'
-		] )
+		], null )
 		
 		assert entity.contentType.value == "application/json"
 //		println entity.content.text
 		assert entity.content.text == '{"first":{"one":1,"two":"2"},"second":"some string"}'
 		
-		entity = reg.encodeJSON( ["first", "second", 3, [map:4] ] )
+		entity = reg.encodeJSON( ["first", "second", 3, [map:4] ], 'text/javascript' )
+		assert entity.contentType.value == "text/javascript"
   		assert entity.content.text == '["first","second",3,{"map":4}]'
   		
-  		entity = reg.encodeJSON {
+  		entity = reg.encodeJSON( {
 			root {
 				first {
 					one = 1
@@ -147,15 +149,15 @@ public class RegistryTest {
 				}
 				second = 'some string'
 			}
-		}
+		}, null )
 		
 //		println entity.content.text
 		assert entity.content.text == '{"root":{"first":{"one":1,"two":"2"},"second":"some string"}}'
 		
-		entity = reg.encodeJSON( '["first","second",3,{"map":4}]' )
+		entity = reg.encodeJSON( '["first","second",3,{"map":4}]',null )
   		assert entity.content.text == '["first","second",3,{"map":4}]'
 		def another = 'second'
-		entity = reg.encodeJSON( "['first','$another',3,{'map':4}]" )
+		entity = reg.encodeJSON( "['first','$another',3,{'map':4}]",null )
   		assert entity.content.text == "['first','second',3,{'map':4}]"
 	}
 	
@@ -168,7 +170,7 @@ public class RegistryTest {
 		assert entity.contentType.elements[0].name == 'application/x-www-form-urlencoded'
 		assert entity.content.text == "p1=one&p2=two&p2=three"
 		
-		entity = enc.encodeForm( "p1=goober&p2=something+else" )
+		entity = enc.encodeForm( "p1=goober&p2=something+else",null )
 		assert entity.contentType.value == 'application/x-www-form-urlencoded'
 		assert entity.content.text == "p1=goober&p2=something+else"
 	}
